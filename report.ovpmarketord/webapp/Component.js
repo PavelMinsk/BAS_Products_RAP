1d309c010dd7edec373c625bb670f4028ee4e6d1
PavelMinsk <pvlmby@gmail.com>
2021-04-13 16:29:36 +0000

    13.04.2021

---
+++ report.ovpmarketord/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/ovp/app/Component'], function(AppComponent) {
+    return AppComponent.extend("productsnamespace.report.ovpmarketord.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
