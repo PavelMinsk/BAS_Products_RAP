c3f8a1782d249ba13f85003088d86d5e201c9c03
PavelMinsk <pvlmby@gmail.com>
2021-05-25 08:28:46 +0000

    25.05.2021

---
+++ manage.products2/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+    return AppComponent.extend("productsnamespace.manage.products2.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
